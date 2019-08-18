ec3c7af078795727b12c1a195c603e90f236c078
Kevin Spies <kevin.spies@gmail.com>
2019-08-18 17:03:29 -0400

    imported react router dom and adding links to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,6 +6,12 @@ function Nav() {
       <a className="navbar-brand" href="/">
         Google Books
       </a>
+      <a className="nav-link" href="/saved">
+        Search
+      </a>
+      <a className="nav-link" href="/saved">
+        Saved
+      </a>
     </nav>
   );
 }
